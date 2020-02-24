c3d4358666707df2922047d81711bd95e123e4d5
Peter Li <bl2403@nyu.edu>
2020-02-24 10:19:16 -0500

    add a room

--- settings.py
+++ settings.py
@@ -33,7 +33,12 @@ LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 
-ROOMS = []
+ROOMS = [
+    dict(
+        name='econ_lab',
+        display_name='CESS Lab',
+    ),
+]
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
